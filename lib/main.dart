e5c180e495d2584c8887775b33903cb039ee1b5a
a <ruan65@gmail.com>
2022-10-29 00:50:50 +0300

    step-1 StreamBuilder .debounceTime()

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,14 +20,29 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final subject = useMemoized(() => BehaviorSubject<String>(), [key]);
+
+    useEffect(() => subject.close, [subject]);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: StreamBuilder<String>(
+          stream: subject.stream
+              .distinct()
+              .debounceTime(const Duration(seconds: 1)),
+          initialData: 'Please start typing...',
+          builder: (context, snapshot) => Text(snapshot.requireData ?? ''),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField(
+          onChanged: subject.sink.add,
+        ),
       ),
     );
   }
