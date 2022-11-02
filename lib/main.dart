94a44d4d4492dda844c494ee7422cc9e86212d83
a <ruan65@gmail.com>
2022-11-03 00:45:42 +0300

    step-3 combine latest

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
-import 'package:testing_rxdart_examples/views/home_page.dart';
+import 'package:rxdart/rxdart.dart';
+import 'dart:developer' as devtools show log;
 
-void main() {
-  runApp(const MyApp());
+extension Log on Object {
+  void log() => devtools.log(toString());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.orange,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+void testIt() async {
+  final stream1 = Stream.periodic(
+      const Duration(seconds: 1), (count) => 'Stream 1, count = $count');
+  final stream2 = Stream.periodic(
+      const Duration(seconds: 3), (count) => 'Stream 2, count = $count');
+
+  final combinedStream = Rx.combineLatest2(
+      stream1, stream2, (one, two) => 'One = ($one), two=($two)');
+
+      await for(final value in combinedStream) {
+        value.log();
+      }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    testIt();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
       ),
-      home: const HomePage(),
+      body: Container(),
     );
   }
 }
