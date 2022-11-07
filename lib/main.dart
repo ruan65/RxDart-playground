d976cc02f44b1833af235c85dbb14f9eff242a1c
a <ruan65@gmail.com>
2022-11-08 01:21:19 +0300

    step-10 concat streams with future builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,166 +1,64 @@
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:rxdart/rxdart.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      primarySwatch: Colors.orange,
+  runApp(
+    MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomePage(),
     ),
-    home: const HomePage(),
-  ));
+  );
 }
 
-typedef AsyncSnapshotBuilerCallback<T> = Widget Function(
-  BuildContext context,
-  T? value,
-);
-
-class AcyncSnapshotBuilder<T> extends StatelessWidget {
-  final Stream<T> stream;
-  final AsyncSnapshotBuilerCallback<T>? onNone;
-  final AsyncSnapshotBuilerCallback<T>? onWaiting;
-  final AsyncSnapshotBuilerCallback<T>? onActive;
-  final AsyncSnapshotBuilerCallback<T>? onDone;
-
-  const AcyncSnapshotBuilder({
-    super.key,
-    required this.stream,
-    this.onNone,
-    this.onWaiting,
-    this.onActive,
-    this.onDone,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<T>(
-      stream: stream,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            final callback = onNone ?? (_, __) => const SizedBox();
-            return callback(
-              context,
-              snapshot.data,
-            );
-          case ConnectionState.waiting:
-            final callback =
-                onWaiting ?? (_, __) => const CircularProgressIndicator();
-            return callback(context, snapshot.data);
-          case ConnectionState.active:
-            final callback = onActive ?? (_, __) => const SizedBox();
-            return callback(
-              context,
-              snapshot.data,
-            );
-          case ConnectionState.done:
-            final callback = onDone ?? (_, __) => const SizedBox();
-            return callback(
-              context,
-              snapshot.data,
-            );
-        }
-      },
-    );
-  }
+Stream<String> getNames({
+  required String filePath,
+}) {
+  final names = rootBundle.loadString(filePath);
+  return Stream.fromFuture(names).transform(const LineSplitter());
 }
 
-@immutable
-class Bloc {
-  final Sink<String> setFirstName;
-  final Sink<String> setLastName;
-  final Stream<String> fullName;
+Stream<String> getAllNames() => getNames(filePath: 'assets/texts/cats.txt')
+    .concatWith([getNames(filePath: 'assets/texts/dogs.txt')]);
 
-  static const defaultMsg = 'Both must be provided First and Last names';
-
-  const Bloc._({
-    required this.setFirstName,
-    required this.setLastName,
-    required this.fullName,
-  });
-
-  factory Bloc() {
-    final fns = BehaviorSubject<String>()..startWith('');
-    final lns = BehaviorSubject<String>()..startWith('');
-    Stream<String> fullName =
-        Rx.combineLatest2(fns.stream, lns.stream, (firstName, lastName) {
-      return firstName.isEmpty || lastName.isEmpty
-          ? defaultMsg
-          : '$firstName $lastName';
-    }).startWith(defaultMsg);
-    return Bloc._(
-      setFirstName: fns.sink,
-      setLastName: lns.sink,
-      fullName: fullName,
-    );
-  }
-
-  void dispose() {
-    setFirstName.close();
-    setLastName.close();
-  }
-}
-
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  late final Bloc _bloc;
-  final firstController = TextEditingController();
-  final lastController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc = Bloc();
-  }
-
-  @override
-  void dispose() {
-    _bloc.dispose();
-    firstController.dispose();
-    lastController.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Combine latest with RxDart'),
-        ),
-        body: Column(
-          children: [
-            TextField(
-              controller: firstController,
-              decoration: const InputDecoration(hintText: 'First name'),
-              onChanged: ((value) {
-                _bloc.setFirstName.add(value);
-              }),
-            ),
-            TextField(
-              controller: lastController,
-              decoration: const InputDecoration(hintText: 'Last name'),
-              onChanged: (value) {
-                _bloc.setLastName.add(value);
-              },
-            ),
-            AcyncSnapshotBuilder(
-              stream: _bloc.fullName,
-              onActive: (context, value) => Text(
-                value!,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ],
-        ));
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: FutureBuilder<List<String>>(
+          future: getAllNames().toList(),
+          builder: (context, snapshot) {
+            final names = snapshot.requireData;
+            return Column(
+              children: names.map((e) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                  case ConnectionState.active:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  case ConnectionState.done:
+                    return ListTile(
+                      title: Text(
+                        e,
+                        style: const TextStyle(color: Colors.black),
+                      ),
+                    );
+                }
+              }).toList(),
+            );
+          }),
+    );
   }
 }
