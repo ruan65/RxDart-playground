bfec52bd7083f8a92e4267229da6758c2904fd5b
a <ruan65@gmail.com>
2022-11-27 12:12:45 +0300

    intentional exception

--- lib/dart/count_stream_example.dart
+++ lib/dart/count_stream_example.dart
@@ -1,17 +1,25 @@
 Stream<int> intStream(int count) async* {
   for (int i in Iterable.generate(count)) {
-    yield i + 1;
-    await Future.delayed(const Duration(
-      milliseconds: 50,
-    ));
+    if (i == 5) {
+      throw Exception('Intentional exception');
+    } else {
+      yield i + 1;
+      await Future.delayed(const Duration(
+        milliseconds: 50,
+      ));
+    }
   }
 }
 
 Future<int> countStream(Stream<int> stream) async {
   int res = 0;
-  await for (int i in stream) {
-    res += i;
-    print(res);
+  try {
+    await for (int i in stream) {
+      res += i;
+      print(res);
+    }
+  } catch (ex) {
+    return -1;
   }
   return res;
 }
